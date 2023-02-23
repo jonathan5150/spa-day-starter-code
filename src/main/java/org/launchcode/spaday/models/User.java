51bf55e3135357ba103ca7984a224bef1697ad1c
Jonathan Rainey <jonathanmichael5150@gmail.com>
2023-02-23 15:32:31 -0600

    Almost have studio working

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,12 +1,29 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    private static int nextId = 1;
+
+    private int id;
+
+    @NotBlank
+    @Size(min = 5, max = 15, message = "Username must be between 5 and 15 characters.")
     private String username;
+
+    @Email(message = "Invalid email. Please try again.")
     private String email;
+
+    @NotBlank
+    @Size(min = 6, message = "Password must be at least 6 characters in length.")
     private String password;
 
     public User() {
-
+        this.id = nextId;
+        nextId++;
     }
 
     public User(String username, String email, String password) {
